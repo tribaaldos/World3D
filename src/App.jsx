e7d88ef2323b38624dcfd1d8ffb61c27e6bc1e74
tribaaldos <tribaaldos@gmail.com>
2024-02-21 14:46:57 +0100

    First Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import { Canvas } from '@react-three/fiber'
 import { Sky } from '@react-three/drei'
-import Experience from '../../World3D/src/World/World';
-
+import Experience from './World/World'
 function App() {
   const [count, setCount] = useState(0)
 
